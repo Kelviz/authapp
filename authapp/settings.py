acabebe822e18ce75d5491669b2cad73971e440a
Kelviz <mysviurch@gmail.com>
2024-07-09 20:52:03 +0100

    second commit

--- authapp/settings.py
+++ authapp/settings.py
@@ -4,6 +4,7 @@ from datetime import timedelta
 import dotenv
 from dotenv import load_dotenv
 load_dotenv()
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
